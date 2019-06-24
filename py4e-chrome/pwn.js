dcfa833918ebcea6c07311a871d21e08aa980b04
Piyush Raj <piyush.189303055@muj.manipal.edu>
2019-06-25 00:35:52 +0530

    pwn.js interactive w/ ins

--- py4e-chrome/pwn.js
+++ py4e-chrome/pwn.js
@@ -3,10 +3,26 @@ var obj = JSON.parse(result);
 var keywords = Object.keys(obj);  // if it contains !3, so 3 can't be there, so do something like, 2+1
 var answer = document.getElementById("desired").outerHTML.replace(/<[^>]*>?/gm, '').replace("\n", "");
 
+var bad_const = [];
+var kgood = [];
+
 for (var i = 0; i < keywords.length; i++) {
-    if (keywords[i] != "print"){
-    	console.log(keywords[i]);
+    if (keywords[i].includes("!")){
+    	bad_const.push(keywords[i]);
+    } else {
+    	kgood.push(keywords[i]);
     }
 }
+console.log("Include these :");
+for (var i = 0; i < kgood.length; i++) {
+	console.log(kgood[i]);
+}
 
+console.log("Answer :");
 console.log("print("+answer+")");
+
+
+console.log("Avoid these :");
+for (var i = 0; i < bad_const.length; i++) {
+	console.log(bad_const[i].replace("!", "").replace("=", "").replace(" ", ""));
+}
